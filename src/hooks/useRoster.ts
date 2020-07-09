dfe43fb05ed18a70b458cb0aecec9108bf2be2c1
Jam Risser <jam@codejam.ninja>
2020-07-09 19:08:21 +0000

    Fixed roster

--- src/hooks/useRoster.ts
+++ src/hooks/useRoster.ts
@@ -1,29 +1,16 @@
 import useStateCache from 'use-state-cache';
 import { useEffect } from 'react';
-import _ from 'lodash';
-import useXmpp from './useXmpp';
 import useRosterService from './useRosterService';
+import useXmpp from './useXmpp';
 import { RosterItem } from '../clients';
 
 export default function useRoster(): RosterItem[] | undefined {
+  const rosterService = useRosterService();
   const xmpp = useXmpp();
   const [roster, setRoster] = useStateCache<RosterItem[]>(
     `${xmpp?.fullJid}/roster`,
     []
   );
-  const rosterService = useRosterService();
-  const updateRoster = async (roster: RosterItem[], rosterItem: RosterItem) => {
-    const currentRosterItems: RosterItem[] = _.map(
-      roster,
-      (rosterSubItem: RosterItem) => {
-        return rosterSubItem.jid === rosterItem.jid
-          ? rosterItem
-          : rosterSubItem;
-      }
-    );
-    const rosterItems = new Set([...currentRosterItems, rosterItem]);
-    setRoster([...rosterItems]);
-  };
 
   useEffect(() => {
     let cleanup = () => {};
@@ -31,8 +18,17 @@ export default function useRoster(): RosterItem[] | undefined {
       if (!rosterService) return;
       const roster = await rosterService.getRoster();
       setRoster(roster);
-      cleanup = rosterService!.readRosterPush((rosterItem: RosterItem) => {
-        if (roster) updateRoster(roster, rosterItem);
+      cleanup = rosterService!.readRosterPush((newRosterItem: RosterItem) => {
+        let newRoster = [...roster];
+        const rosterItem = newRoster.find(
+          ({ jid }: RosterItem) => jid === newRosterItem.jid
+        );
+        if (rosterItem) {
+          Object.assign(rosterItem, newRosterItem);
+        } else {
+          newRoster = [...newRoster, newRosterItem];
+        }
+        setRoster(newRoster);
       });
     })().catch(console.error);
     return () => cleanup();
