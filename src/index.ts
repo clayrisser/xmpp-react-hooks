504e8ee27ff412521189fedb51d420e611983c2a
Jam Risser <jam@codejam.ninja>
2020-08-15 03:14:50 +0000

    Formatted code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-export * from '@xmpp-ts/jid';
-export * from '@xmpp/client';
 import Jid from '@xmpp-ts/jid';
 import XMPPError from '@xmpp/error';
 import xmppDebug from '@xmpp/debug';
 import Provider from './Provider';
+
+export * from '@xmpp-ts/jid';
+export * from '@xmpp/client';
 
 export * from './hooks';
 export * from './xmpp';
