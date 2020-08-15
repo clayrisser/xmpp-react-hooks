916a2c600116fb771b1765a51eba359a5761f741
Jam Risser <jam@codejam.ninja>
2020-08-15 03:14:01 +0000

    Improved exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+export * from '@xmpp-ts/jid';
+export * from '@xmpp/client';
+import Jid from '@xmpp-ts/jid';
+import XMPPError from '@xmpp/error';
+import xmppDebug from '@xmpp/debug';
 import Provider from './Provider';
 
 export * from './hooks';
 export * from './xmpp';
 
-export { Provider };
+export { Provider, Jid, xmppDebug, XMPPError };
